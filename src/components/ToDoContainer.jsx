64d281d32f51263c1c0d6b160155d1e5dbbe1bdd
recillah khamala <recillahk@gmail.com>
2022-11-02 18:14:39 +0300

    Fix linters errors

---
+++ src/components/ToDoContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header';
+
+const ToDoContainer = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default ToDoContainer;
